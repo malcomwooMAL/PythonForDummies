4d641b3a72f454dce6d24f2491e210b5f9efd573
malcomwoo <tadashimal@gmail.com>
2025-03-07 13:38:31 -0300

    Inicial

---
+++ Aprendizado/1_FuncoesMath.py
@@ -0,0 +1,28 @@
+# retornar o valor absoluto (converte o valor inteiro para natural)
+print(abs(-5))
+print(abs(-20))
+
+# retorna a representação binária de um valor
+print(bin(256))
+print(bin(8))
+
+#converte um número para um valor inteiro
+variavelInt = 50
+varivelStr = str(variavelInt)
+print(type(varivelStr)) # vai retornar que a variavelStr é do tipo String
+
+#max() retorna o valor máximo dos parâmetros
+print(max(15,8999,770))
+
+#min() retorna o valor mínimo dos parametros
+print(min(15,8999,770))
+
+#int() converte para inteiro mas sem arredondamento
+variavel_float = 17.999
+print(int(variavel_float))
+
+#aninhamento de funções
+z = -999.999
+print(abs(int(z)))
+
+
